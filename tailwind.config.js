a6bcb64e238d87ee5709812e29817a8280a12d64
windistro <hanshavilahf@gmail.com>
2023-04-16 22:27:47 +0700

    a

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
